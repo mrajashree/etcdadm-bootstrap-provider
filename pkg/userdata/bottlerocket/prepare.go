cff3e6b8d9c7801fd0e634e4fed047488a11830f
Guillermo Gaston <gaslor@amazon.com>
2021-08-11 11:18:32 +0000

    Patch certs paths in WriteFiles to point to the dir BR bootstrap container expects for etcdadm

--- pkg/userdata/bottlerocket/prepare.go
+++ pkg/userdata/bottlerocket/prepare.go
@@ -1,14 +1,36 @@
 package bottlerocket
 
 import (
+	"path/filepath"
+
 	"github.com/go-logr/logr"
 	"github.com/mrajashree/etcdadm-bootstrap-provider/pkg/userdata"
+	"sigs.k8s.io/cluster-api/bootstrap/kubeadm/api/v1alpha3"
+)
+
+const (
+	orgCertsPath = "/etc/etcd/pki"
+	newCertsPath = "/var/lib/etcd/pki"
 )
 
 func prepare(input *userdata.BaseUserData) {
 	input.Header = cloudConfigHeader
 	input.WriteFiles = append(input.WriteFiles, input.AdditionalFiles...)
 	input.SentinelFileCommand = sentinelFileCommand
+	patchCertPaths(input)
+}
+
+func patchCertPaths(input *userdata.BaseUserData) {
+	// hacky. new array bc I need to update the original object and these are not pointers
+	files := make([]v1alpha3.File, 0, len(input.WriteFiles))
+	for _, file := range input.WriteFiles {
+		if filepath.Dir(file.Path) == orgCertsPath {
+			file.Path = filepath.Join(newCertsPath, filepath.Base(file.Path))
+		}
+		files = append(files, file)
+	}
+
+	input.WriteFiles = files
 }
 
 func logIgnoredFields(input *userdata.BaseUserData, log logr.Logger) {
